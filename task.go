572ec94712b7013f9a30c6562ee75cc44fd89296
kotjuz <rkotarski10@gmail.com>
2025-10-13 11:24:33 +0200

    feat: added List, Add, Toggle funcs to task.go

--- task.go
+++ task.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Task struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
@@ -26,4 +28,24 @@ func NewTaskStore(filePath string) (*TaskStore, error) {
 	}
 
 	return &TaskStore{filePath: filePath, tasks: tasks, nextID: nextID}, nil
+}
+
+func (s *TaskStore) List() []*Task {
+	return s.tasks
+}
+
+func (s *TaskStore) Add(taskName string) error {
+	s.tasks = append(s.tasks, &Task{ID: s.nextID, Name: taskName, Done: false})
+	s.nextID += 1
+	return SaveTasks(s.filePath, s.tasks)
+}
+
+func (s *TaskStore) Toggle(id int) error {
+	for _, t := range s.tasks {
+		if t.ID == id {
+			t.Done = !t.Done
+			return SaveTasks(s.filePath, s.tasks)
+		}
+	}
+	return fmt.Errorf("task %d not found", id)
 }
