faf532c9dc5e2edfb8fb1e68dd2289fbbe54cdc2
kotjuz <rkotarski10@gmail.com>
2025-10-21 21:24:34 +0200

    feat: shortened toggle to tg in stoggle subcommand

--- cmd/toggle.go
+++ cmd/toggle.go
@@ -11,7 +11,7 @@ import (
 var ToggleTask func(int) error
 
 var toggleCmd = &cobra.Command{
-	Use:   "toggle [task ID]",
+	Use:   "tg [task ID]",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
