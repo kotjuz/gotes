17e003a05d6f8ff15428b5228982451c23120880
kotjuz <rkotarski10@gmail.com>
2025-11-05 18:01:08 +0100

    feat: added new subcommand priority

---
+++ cmd/priority.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var SetPriority func(int, int) error
+
+var priorityCmd = &cobra.Command{
+	Use:   "priority [task ID][priority ID]",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if SetPriority == nil {
+			fmt.Println("priority: store not initialized")
+			return
+		}
+		if len(args) < 2 {
+
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(priorityCmd)
+}
